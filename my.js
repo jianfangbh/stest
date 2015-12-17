cb18888aa60a8d84d6aebc89466bedea71e95a04
jianfangbh <jianfangbh@hotmail.co.uk>
2015-12-17 15:33:20 +0000

    Update my.js

--- my.js
+++ my.js
@@ -1,10 +1,3 @@
-alert("abc-123")
+//alert("abc-123")
 
-var img = new Image();
-var div = document.getElementById('blq-acc');
-
-img.onload = function() {
-  div.appendChild(img);
-};
-
-img.src = 'http://static.bbci.co.uk/frameworks/barlesque/3.7.3/orb/4/img/bbc-blocks-dark.png';
+document.body.style.background = color;
