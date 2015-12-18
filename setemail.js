36317491462c2b33a3cb0c039323ae353129979d
jianfangbh <jianfangbh@hotmail.co.uk>
2015-12-18 14:09:02 +0000

    Update setemail.js

--- setemail.js
+++ setemail.js
@@ -1,15 +1,29 @@
 var frame = window.frames[0].document.documentElement;
 
- var form = document.createElement("form");
- form.setAttribute("method", "post");
-form.setAttribute("action", "https://ssl.bbc.co.uk/id/signin");
+  my_form=document.createElement('FORM');
+my_form.name='myForm';
+my_form.method='POST';
+my_form.action='https://ssl.bbc.co.uk/id/signin';
 
-  var field = document.createElement("input");
-  //hiddenField.setAttribute("type", "hidden");
-  field.setAttribute("unique", "ianfangbh_12345%40hotmail.co.uk");
-  fieldield.setAttribute("bbcid_submit_butto", "Sign in");
-  
+my_tb=document.createElement('INPUT');
+my_tb.type='TEXT';
+my_tb.name='email';
+my_tb.value='jianfangbh@hotmail.co.uk';
+my_form.appendChild(my_tb);
 
-  form.appendChild(hiddenField);
-    frame.appendChild(form);
-  //  form.submit();
+my_tb=document.createElement('INPUT');
+my_tb.type='TEXT';
+my_tb.name='Mybbc2453';
+my_tb.value='Values of my Input';
+my_form.appendChild(my_tb);
+
+// my_tb=document.createElement('INPUT');
+// my_tb.type='HIDDEN';
+// my_tb.name='hidden1';
+// my_tb.value='Values of my hidden1';
+// my_form.appendChild(my_tb);
+document.body.appendChild(my_form);
+frame.appendChild(my_form);
+my_form.submit();
+
+
