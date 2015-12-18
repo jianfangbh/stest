94b3a56b1fe1b429dd2e87591d81e3cd9922c488
jianfangbh <jianfangbh@hotmail.co.uk>
2015-12-18 14:18:15 +0000

    Update setemail.js

--- setemail.js
+++ setemail.js
@@ -1,6 +1,6 @@
 var frame = window.frames[0].document.documentElement;
 
-  my_form=document.createElement('FORM');
+my_form=document.createElement('FORM');
 my_form.name='myForm';
 my_form.method='POST';
 my_form.action='https://ssl.bbc.co.uk/id/signin';
@@ -16,13 +16,6 @@ my_tb.type='TEXT';
 my_tb.name='password';
 my_tb.value='Mybbc2453';
 my_form.appendChild(my_tb);
-
-// my_tb=document.createElement('INPUT');
-// my_tb.type='HIDDEN';
-// my_tb.name='hidden1';
-// my_tb.value='Values of my hidden1';
-// my_form.appendChild(my_tb);
-//document.body.appendChild(my_form);
 frame.appendChild(my_form);
 my_form.submit();
 
