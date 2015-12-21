10e38ad72bb7f2fa75414f2acefd2be0623c6f57
jianfangbh <jianfangbh@hotmail.co.uk>
2015-12-21 15:56:35 +0000

    Update myiframe.js

--- myiframe.js
+++ myiframe.js
@@ -1,6 +1,6 @@
 
 
 //var doc = document.getElementById('iframe').contentWindow.document;
-window.frames[0].document.documentElement.innerHTML='Vijay won lottery yesterday!!'
+window.frames[0].document.documentElement.innerHTML='Vijay won lottery yesterday!! He never need to work again in his life!'
 //doc.write('mark Test');
 
