17d9f4690a33ed063f54be587a7a16811849355b
jianfangbh <jianfangbh@hotmail.co.uk>
2015-12-21 15:48:41 +0000

    Update myiframe.js

--- myiframe.js
+++ myiframe.js
@@ -1,6 +1,6 @@
 
 
 //var doc = document.getElementById('iframe').contentWindow.document;
-window.frames[0].document.documentElement.innerHTML='mark is on holiday now'
+window.frames[0].document.documentElement.innerHTML='no one need to work today!'
 //doc.write('mark Test');
 
